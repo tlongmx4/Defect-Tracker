c39c90e771d1333c2d14bb6707fad36f4eb1172c
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-04-13 14:58:12 -0400

    Add transactional database session fixture to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ import os
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from app.db.base import Base
 
 TEST_DATABASE_URL = os.environ["TEST_DATABASE_URL"]
@@ -20,5 +21,19 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_
 def setup_schema():
     Base.metadata.create_all(bind=test_engine)
     yield
+
+@pytest.fixture()
+def setup_session(setup_schema):
+    connection = test_engine.connect()
+    trans = connection.begin()
+    session = Session(
+            bind=connection, join_transaction_mode="create_savepoint"
+        ) 
+    yield session
+    session.close()
+    trans.rollback()
+    connection.close()
+
+
 
 
