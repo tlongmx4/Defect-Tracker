801eb2e6080ca807f9d48406231ff960f9e5e4be
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-05-08 11:38:49 -0400

    fix(backend): typo in main.py

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(title="Defect Tracker API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localthost:5173"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
